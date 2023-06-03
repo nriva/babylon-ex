17677046e7ee1f6daee35cba34ed00b7e3a22582
Nicola <nicola.riva1@gmail.com>
2023-06-03 11:15:58 +0200

    Upgrade versions

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import { Engine } from '@babylonjs/core/Engines/engine'
 import { Scene } from '@babylonjs/core/scene'
 import "@babylonjs/core/Helpers/sceneHelpers"
-import { Mesh } from '@babylonjs/core/Meshes'
+import { Mesh, MeshBuilder } from '@babylonjs/core/Meshes'
 import { Vector3, StandardMaterial, Color3, DynamicTexture } from '@babylonjs/core'
 import { createScene } from './createScene';
 
@@ -27,24 +27,24 @@ function showWorldAxis(size: number) {
       material.diffuseTexture = dynamicTexture;
   return plane;
    };
-  var axisX = Mesh.CreateLines("axisX", [ 
+  var axisX = MeshBuilder.CreateLines("axisX", {points: [ 
     Vector3.Zero(), new Vector3(size, 0, 0), new Vector3(size * 0.95, 0.05 * size, 0), 
     new Vector3(size, 0, 0), new Vector3(size * 0.95, -0.05 * size, 0)
-    ], scene);
+    ]}, scene);
   axisX.color = new Color3(1, 0, 0);
   var xChar = makeTextPlane("X", "red", size / 10);
   xChar.position = new Vector3(0.9 * size, -0.05 * size, 0);
-  var axisY = Mesh.CreateLines("axisY", [
+  var axisY = MeshBuilder.CreateLines("axisY", {points: [
       Vector3.Zero(), new Vector3(0, size, 0), new Vector3( -0.05 * size, size * 0.95, 0), 
       new Vector3(0, size, 0), new Vector3( 0.05 * size, size * 0.95, 0)
-      ], scene);
+      ]}, scene);
   axisY.color = new Color3(0, 1, 0);
   var yChar = makeTextPlane("Y", "green", size / 10);
   yChar.position = new Vector3(0, 0.9 * size, -0.05 * size);
-  var axisZ = Mesh.CreateLines("axisZ", [
+  var axisZ = MeshBuilder.CreateLines("axisZ", {points:[
       Vector3.Zero(), new Vector3(0, 0, size), new Vector3( 0 , -0.05 * size, size * 0.95),
       new Vector3(0, 0, size), new Vector3( 0, 0.05 * size, size * 0.95)
-      ], scene);
+      ]}, scene);
   axisZ.color = new Color3(0, 0, 1);
   var zChar = makeTextPlane("Z", "blue", size / 10);
   zChar.position = new Vector3(0, 0.05 * size, 0.9 * size);
